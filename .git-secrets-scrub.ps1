102404f660b69c2c27b6ade1f3d0d32c4d12252e
workabdurrahman-lgtm <workabdurrahman@gmail.com>
2025-09-14 17:31:08 +0700

    remove_ps_scrub

--- .git-secrets-scrub.ps1
+++
@@ -1,9 +0,0 @@
-$ErrorActionPreference = 'Stop'
-$p = 'src/main.cpp'
-if (Test-Path $p) {
-  $t = Get-Content $p -Raw
-  $t = $t -replace 'static const wchar_t\*\s*kGoogleClientSecret\s*=\s*L"[^"]*";', 'static const wchar_t* kGoogleClientSecret = L"";'
-  $t = $t -replace 'static const wchar_t\*\s*kGoogleClientId\s*=\s*L"[^"]*";', 'static const wchar_t* kGoogleClientId = L"example.apps.googleusercontent.com";'
-  $t = $t -replace 'static const wchar_t\*\s*kFirebaseApiKey\s*=\s*L"[^"]*";', 'static const wchar_t* kFirebaseApiKey = L"";'
-  Set-Content -NoNewline -Path $p -Value $t
-}
